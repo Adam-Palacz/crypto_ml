1c7a40289f94608f1ce3f051479203a86d230156
Adam-Palacz <adam.palacz96@gmail.com>
2021-05-01 15:37:15 +0200

    share trading simulation as API

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-from data_to_sql import print_historical_prices, print_current_prices, print_predict_prices
+from data_to_sql import print_historical_prices, print_current_prices, print_predict_prices, print_buys_simulator
 
 app = Flask(__name__)
 
@@ -17,6 +17,11 @@ def get_crypto_history(symbol, interval):
 @app.route('/crypto/<symbol>/<interval>/predict/', methods=['GET'])
 def get_predict_price(symbol, interval):
     return jsonify(print_predict_prices(symbol, interval))
+
+
+@app.route('/crypto/<symbol>/<interval>/<int:money>/simulation/', methods=['GET'])
+def get_buys_simulation(symbol, interval, money):
+    return jsonify(print_buys_simulator(symbol, interval, money))
 
 
 if __name__ == '__main__':
--- trading_simulation.py
+++ trading_simulation.py
@@ -1,9 +1,9 @@
 from predict_model import trade_move, mean_model_predict
+import pandas as pd
 
-
-def only_buys_simulator(symbol, interval):
+def only_buys_simulator(symbol, interval, money):
     trade = trade_move(mean_model_predict, symbol, interval)
-    money = int(input("Input how much money do you want to spend for each transaction: "))
+    money = int(money)
     money_spend = 0
     buys = 0
     bought_coins = 0
@@ -15,20 +15,19 @@ def only_buys_simulator(symbol, interval):
     coin_price = trade['mean'].iloc[-1]
     fortune = (bought_coins * coin_price).round(2)
     profit = (fortune - money_spend).round(2)
-    transactions = f"""
-Buys number: {buys}
-Bought coins: {bought_coins}
-Money spend: {money_spend}
-Current coin price: {coin_price}
-    """
-    final_fortune = f"Fortune: {fortune}\nProfit: {profit}"
-    return f"{transactions}\n{final_fortune}"
+    buys_simulator_df = pd.DataFrame(data={
+        'Buys number': buys,
+        'Bought coins': bought_coins,
+        'Money spend': money_spend,
+        'Current coin price': coin_price,
+        'Fortune': fortune,
+        'Profit': profit,
+    }, index=[0])
+    return buys_simulator_df
 
 
-def buys_sales_simulator(symbol, interval):
+def buys_sales_simulator(symbol, interval, money_buys, money_sales):
     trade = trade_move(mean_model_predict, symbol, interval)
-    money_buys = int(input("Input how much money do you want to spend for each transaction: "))
-    money_sales = int(input("Input for how much money do you want to sell coins: "))
     money_spend = 0
     buys = 0
     bought_coins = 0
@@ -45,16 +44,17 @@ def buys_sales_simulator(symbol, interval):
     coin_price = trade['mean'].iloc[-1]
     fortune = (bought_coins * coin_price).round(2)
     profit = (fortune - money_spend).round(2)
-    transactions = f"""
-Buys number: {buys}
-Sales number: {sales}
-Bought coins: {bought_coins}
-Money spend: {money_spend}
-Current coin price: {coin_price}
-    """
-    final_fortune = f"Fortune: {fortune}\nProfit: {profit}"
-    return f"{transactions}\n{final_fortune}"
+    buys_sales_simulator_df = pd.DataFrame(data={
+        'Buys number': buys,
+        'Sales number': sales,
+        'Bought coins': bought_coins,
+        'Money spend': money_spend,
+        'Current coin price': coin_price,
+        'Fortune': fortune,
+        'Profit': profit,
+    }, index=[0])
+    return buys_sales_simulator_df
 
 
 
-# print(buys_sales_simulator('BTCUSDT', '1d'))
+# print(buys_sales_simulator('BTCUSDT', '1d', 100, 50))
